72ccbbf27e157de14a3f5c0ddf1bd24678aebe2d
Austin Hurst <mynameisaustinhurst@gmail.com>
2020-11-14 21:21:41 -0400

    Initial commit of working pipeline

---
+++ README.md
@@ -0,0 +1,35 @@
+# TraceLab EEG Preprocessing Pipeline
+
+## Processing Steps
+
+1. Repair mislabeled or duplicated event triggers
+2. Remove line noise from the data using CleanLine
+3. Perform robust rereferencing and re-interpolate bad channels using PyPREP
+4. Apply highpass (1 Hz) and lowpass (50 Hz) filters to the data
+5. Use ICA and EOG channel data to detect and remove components of the signal associated with blinks
+6. Save the preprocessed data files to EDF+
+
+## Requirements
+
+- Python 3.7 (newer version may work, but are untested)
+- A reasonably fast computer with at least 8 GB RAM
+- The `pipenv` package
+
+## Instructions
+
+1. First, copy or move the contents of the BIDS-formatted dataset to the project's `bids` folder.
+2. Navigate to the project folder in a terminal window.
+3. Run `pipenv install` at the terminal prompt (NOTE: if using a Python version other than 3.7, you may need to modify the project's Pipfile accordingly first). This will create a clean virtual environment for the project and install all the pipeline's dependencies within it.
+4. When you're ready, run `pipenv run python eeg_pipeline.py` in the terminal to start the process.
+5. Wait for several hours as the pipeline completes!
+
+**NOTE:** Running the pipeline will consume much of your system memory and may make your computer slow or unresponsive, so it may be a good idea to run it overnight.
+
+
+## Output
+
+The pipeline produces three forms of output:
+
+1. The preprocessed EEG data in EDF+ format, located in the `output/eeg` folder
+2. Plots visualizing the effects of the preprocessing steps, located in the `output/plots` folder
+3. Data on the preprocessing for all particpants, located in the `output/prep_info.csv` file
---
+++ eeg_pipeline.py
@@ -0,0 +1,366 @@
+#########################################
+## TraceLab EEG Preprocessing Pipeline ##
+#########################################
+
+# By: Austin Hurst
+
+# Based on the 'run_full_prep.py' example from PyPREP (MIT Licence)
+
+
+### Import required libraries #################################################
+
+import os
+import csv
+import shutil
+import binascii
+import collections
+
+import mne
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+from mne.preprocessing import ICA
+from mne_bids import BIDSPath, read_raw_bids
+from pyprep.prep_pipeline import PrepPipeline
+from pyprep.find_noisy_channels import NoisyChannels
+from pyprep.removeTrend import removeTrend
+from pyprep.reference import Reference
+
+from utils.save_edf import write_mne_edf
+
+
+### Set general parameters ####################################################
+
+matplotlib.use('Qt5Agg') # Only needed to avoid issues with ptpython
+mne.set_log_level("WARNING")
+seed = 530453080 # arbitrary, needed for results to be consistent between runs
+
+task = 'tracelab'
+datatype = 'eeg'
+bids_root = 'bids'
+output_root = 'output'
+outdir = os.path.join(output_root, 'eeg')
+plotdir = os.path.join(output_root, 'plots')
+info_file = os.path.join(output_root, 'prep_info.csv')
+max_interpolated = 0.25 # 25%
+
+outfile_fmt = 'sub-{0}_eeg_prepped.edf'
+
+
+def save_psd_plot(id_num, suffix, path, dat):
+
+    plot_path = os.path.join(path, "sub-{0}_{1}.pdf".format(id_num, suffix))
+    psd = dat.plot_psd(fmax = 100, show = False)
+    psd.set_size_inches(6, 3)
+    psd.savefig(plot_path, bbox_inches = 'tight')
+    plt.close()
+
+
+def save_channel_plot(id_num, suffix, path, dat):
+
+    plot_path = os.path.join(path, "sub-{0}_{1}.png".format(id_num, suffix))
+    ch_plot = dat.plot(
+		n_channels=37, 
+		duration=30, 
+		scalings={"eeg": 1.5e-4, "eog": 5e2, "emg": 25e2},
+		start=500
+	)
+    ch_plot.savefig(plot_path, bbox_inches = 'tight')
+    plt.close()
+
+
+def save_ica_plots(id_num, path, dat, ica, eog_scores):
+
+    plot_path = os.path.join(path, "sub-{0}_ica_scores.pdf".format(id_num))
+    ica_plot1 = ica.plot_scores(eog_scores, show = False)
+    ica_plot1.savefig(plot_path, bbox_inches = 'tight')
+    plt.close()
+
+    ica_plot2 = ica.plot_properties(dat, picks = ica.exclude, show = False)
+    n = 1
+    for p in ica_plot2:
+        plot_path = os.path.join(path, "sub-{0}_ica_{1}.pdf".format(id_num, n))
+        p.savefig(plot_path, bbox_inches = 'tight')
+        plt.close()
+        n += 1
+
+    plot_path = os.path.join(path, "sub-{0}_ica_sources.png".format(id_num))
+    ica_plot3 = ica.plot_sources(dat, show = False)
+    ica_plot3.savefig(plot_path, bbox_inches = 'tight')
+    plt.close()
+
+
+def preprocess_eeg(id_num, random_seed=None):
+    
+    # Set important variables
+    bids_path = BIDSPath(id_num, task=task, datatype=datatype, root=bids_root)
+    plot_path = os.path.join(plotdir, "sub_{0}".format(id_num))
+    if os.path.exists(plot_path):
+        shutil.rmtree(plot_path)
+    os.mkdir(plot_path)
+    if not random_seed:
+        random_seed = int(binascii.b2a_hex(os.urandom(4)), 16)
+    id_info = {"id": id_num, "random_seed": random_seed}
+    
+
+    ### Load and prepare EEG data #############################################
+    
+    header = "### Processing sub-{0} (seed: {1}) ###".format(id_num, random_seed)
+    print("\n" + "#" * len(header))
+    print(header)
+    print("#" * len(header) + "\n")
+
+    # Load EEG data
+    raw = read_raw_bids(bids_path, verbose=True)
+
+    # Add a montage to the data
+    montage_kind = "standard_1005"
+    montage = mne.channels.make_standard_montage(montage_kind)
+    mne.datasets.eegbci.standardize(raw)
+    raw.set_montage(montage)
+
+    # Extract some info
+    eeg_index = mne.pick_types(raw.info, eeg=True, eog=False, meg=False)
+    ch_names = raw.info["ch_names"]
+    ch_names_eeg = list(np.asarray(ch_names)[eeg_index])
+    sample_rate = raw.info["sfreq"]
+
+    # Make a copy of the data
+    raw_copy = raw.copy()
+    raw_copy.load_data()
+
+    # Trim duplicated data (only needed for sub-005)
+    file_starts = [a for a in raw_copy.annotations if a['description'] == "100"]
+    if len(file_starts):
+        duplicate_start = file_starts[0]['onset']
+        raw_copy.crop(tmax=duplicate_start)
+
+    # Make backup of EOG, EMG, & trigger channels to re-append after PREP
+    raw_other = raw_copy.copy()
+    raw_other.pick_types(eog=True, emg=True, stim=True)
+
+    # Prepare copy of raw data for PREP
+    raw_copy.pick_types(eeg=True)
+
+    # Plot data prior to any processing
+    save_psd_plot(id_num, "psd_0_raw", plot_path, raw_copy)
+    save_channel_plot(id_num, "ch_0_raw", plot_path, raw_copy)
+
+
+    ### Clean up events #######################################################
+    
+    print("\n\n=== Processing Event Annotations... ===\n")
+
+    name_map = {
+        '1': "stim_on",
+        '2': "red_on",
+        '3': "trace_start",
+        '4': "trace_end",
+        '10': "accuracy_submit",
+        '20': "vividness_submit",
+    }
+    doubled = []
+    wrong_label = []
+    new_onsets = []
+    new_durations = []
+    new_descriptions = []
+
+    # Find and flag any duplicate triggers
+    annot = raw_copy.annotations
+    trigger_count = len(annot)
+    for i in range(1, trigger_count-1):
+        a = annot[i]
+        on_last = i+1 == trigger_count
+        prev_trigger = annot[i-1]['description']
+        next_onset = annot[i+1]['onset'] if not on_last else a['onset'] + 100
+        # Determine whether duplicates are doubles or mislabeled
+        if a['description'] == prev_trigger:
+            if (next_onset - a['onset']) < 0.002:
+                doubled.append(a)
+            else:
+                wrong_label.append(a)
+
+    # Rename annotations to have meaningful names & fix duplicates
+    for a in raw_copy.annotations:
+        if a in doubled or a['description'] not in name_map.keys():
+            continue
+        if a in wrong_label:
+            if a['description'] == "10":
+                a['description'] = str(int(a['description']) + 10)
+            else:
+                a['description'] = str(int(a['description']) + 1)
+        new_onsets.append(a['onset'])
+        new_durations.append(a['duration'])
+        new_descriptions.append(name_map[a['description']])
+
+    # Replace old annotations with new fixed ones
+    if len(annot):
+        new_annot = mne.Annotations(
+            new_onsets, new_durations, new_descriptions,
+            orig_time=raw_copy.annotations[0]['orig_time']
+        )
+        raw_copy.set_annotations(new_annot)
+
+    # Check annotations to verify we have equal numbers of each
+    counts = collections.Counter(raw_copy.annotations.description)
+    print(counts)
+    
+    # Get info
+    id_info['annot_doubled'] = len(doubled)
+    id_info['annot_wrong'] = len(wrong_label)
+    
+    count_vals = [n for n in counts.values() if n != counts['vividness_submit']]
+    id_info['equal_triggers'] = all(x == count_vals[0] for x in count_vals)
+    id_info['stim_on'] = counts['stim_on']
+    id_info['red_on'] = counts['red_on']
+    id_info['trace_start'] = counts['trace_start']
+    id_info['trace_end'] = counts['trace_end']
+    id_info['acc_submit'] = counts['accuracy_submit']
+    id_info['vivid_submit'] = counts['vividness_submit']
+
+    
+    ### Run components of PREP manually #######################################
+    
+    print("\n\n=== Performing CleanLine... ===")
+
+    # Try to remove line noise using CleanLine approach
+    linenoise = np.arange(60, sample_rate / 2, 60)
+    EEG_raw = raw_copy.get_data() * 1e6
+    EEG_new = removeTrend(EEG_raw, sample_rate=raw.info["sfreq"])
+    EEG_clean = mne.filter.notch_filter(
+        EEG_new,
+        Fs=raw.info["sfreq"],
+        freqs=linenoise,
+        filter_length="10s",
+        method="spectrum_fit",
+        mt_bandwidth=2,
+        p_value=0.01,
+    )
+    EEG_final = EEG_raw - EEG_new + EEG_clean
+    raw_copy._data = EEG_final * 1e-6
+    del linenoise, EEG_raw, EEG_new, EEG_clean, EEG_final
+    
+    # Plot data following cleanline
+    save_psd_plot(id_num, "psd_1_cleanline", plot_path, raw_copy)
+    save_channel_plot(id_num, "ch_1_cleanline", plot_path, raw_copy)
+
+    # Perform robust re-referencing
+    prep_params = {
+        "ref_chs": ch_names_eeg,
+        "reref_chs": ch_names_eeg
+    }
+    reference = Reference(
+        raw_copy,
+        prep_params,
+        ransac=True,
+        random_state=seed
+    )
+    print("\n\n=== Performing Robust Re-referencing... ===\n")
+    reference.perform_reference()
+
+    # Plot data following robust re-reference
+    save_psd_plot(id_num, "psd_2_reref", plot_path, reference.raw)
+    save_channel_plot(id_num, "ch_2_reref", plot_path, reference.raw)
+
+    # Re-append removed EMG/EOG/trigger channels
+    raw_prepped = reference.raw.add_channels([raw_other])
+    
+    # Get info
+    initial_bad = reference.noisy_channels_original["bad_all"]
+    id_info['initial_bad'] = " ".join(initial_bad)
+    id_info['num_initial_bad'] = len(initial_bad)
+    
+    interpolated = reference.interpolated_channels
+    id_info['interpolated'] = " ".join(interpolated)
+    id_info['num_interpolated'] = len(interpolated)
+    
+    remaining_bad = reference.still_noisy_channels
+    id_info['remaining_bad'] = " ".join(remaining_bad)
+    id_info['num_remaining_bad'] = len(remaining_bad)
+
+    # Print re-referencing info
+    print("\nRe-Referencing Info:")
+    print(" - Bad channels original: {0}".format(initial_bad))
+    print(" - Bad channels after re-referencing: {0}".format(interpolated))
+    print(" - Bad channels after interpolation: {0}".format(remaining_bad))
+
+    # Check if too many channels were interpolated for the participant
+    prop_interpolated = len(reference.interpolated_channels) / len(ch_names_eeg)
+    e = "### Too many interpolated channels for sub-{0} ({1}), skipping... ###"
+    if max_interpolated < prop_interpolated:
+        print("\n")
+        print(e.format(id_num, len(reference.interpolated_channels)))
+        print("\n")
+        return id_info
+
+
+    ### Filter data and apply ICA to remove blinks ############################
+
+    # Apply highpass & lowpass filters
+    print("\n\n=== Applying Highpass & Lowpass Filters... ===")
+    filt_raw = raw_prepped.copy()
+    filt_raw.filter(1.0, 50.0, fir_design='firwin')
+
+    # Plot data following frequency filters
+    save_psd_plot(id_num, "psd_3_filtered", plot_path, filt_raw)
+    save_channel_plot(id_num, "ch_3_filtered", plot_path, filt_raw)
+
+    # Perform ICA using EOG data on eye blinks
+    print("\n\n=== Removing Blinks Using ICA... ===\n")
+    ica = ICA(n_components=20, random_state=seed, method='picard')
+    ica.fit(filt_raw, decim=5)
+    eog_indices, eog_scores = ica.find_bads_eog(filt_raw)
+    ica.exclude = eog_indices
+
+    # Plot ICA info & diagnostics before removing from signal
+    save_ica_plots(id_num, plot_path, filt_raw, ica, eog_scores)
+
+    # Remove eye blink independent components based on ICA
+    ica.apply(filt_raw)
+
+    # Plot data following ICA
+    save_psd_plot(id_num, "psd_4_ica", plot_path, filt_raw)
+    save_channel_plot(id_num, "ch_4_ica", plot_path, filt_raw)
+
+
+    ### Write preprocessed data to new EDF ####################################
+
+    outpath = os.path.join(outdir, outfile_fmt.format(id_num))
+    write_mne_edf(outpath, filt_raw)
+    
+    print("\n\n### sub-{0} complete! ###\n\n".format(id_num))
+    
+    return id_info
+    
+
+
+### Run loop for all IDs ######################################################
+
+if not os.path.isdir(output_root):
+    os.mkdir(output_root)
+
+if not os.path.isdir(outdir):
+    os.mkdir(outdir)
+
+if not os.path.isdir(plotdir):
+    os.mkdir(plotdir)
+
+ids = []
+for sub in os.listdir(bids_root):
+    if "sub-" in sub:
+        ids.append(sub.split("-")[1])
+
+first_id = True
+with open(info_file, 'w', newline='') as outfile:
+    writer = csv.writer(outfile)
+    for sub in ids:
+        outpath = os.path.join(outdir, outfile_fmt.format(sub))
+        if os.path.exists(outpath):
+            print(" - '{0}' already exists, skipping...\n".format(outpath))
+            continue
+        info = preprocess_eeg(sub, random_seed=seed)
+        if first_id:
+            writer.writerow(list(info.keys()))
+            first_id = False
+        writer.writerow(list(info.values()))
